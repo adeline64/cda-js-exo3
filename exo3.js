ccf27f7b6d512651b9da7874f5306ef8e0931f7d
Adeline64 <adeline.lacabanne@gmail.com>
2022-09-14 16:38:42 +0200

    projet pour apprendre le javascript/typescript realiser en cda simplon occitanie

---
+++ exo3.js
@@ -0,0 +1,18 @@
+const apprenants = [
+    "Violet Campbell",
+    "Curry Dunlap",
+    "Loretta Wolf",
+    "Elliott Rodriquez",
+    "Daphne Park",
+    "Estelle Rivas"
+];
+
+// Afficher cette liste d'apprenants dans la liste UL de la page HTML
+
+            let liste = document.getElementById('liste');
+            console.log(liste)
+            for(let liste in apprenants) {
+                let liste = document.createElement('liste');
+            };
+            
+       
